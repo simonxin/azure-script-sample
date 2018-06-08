f8b11c1bae71c9b6d40d5b5d66e796ac2e5e6aa8
simonxin <34259289+simonxin@users.noreply.github.com>
2018-06-08 08:46:00 +0800

    pagefilechange

---
+++ setpagefile.ps1
@@ -0,0 +1,79 @@
+﻿function Set-PageFile
+{
+    [CmdletBinding(SupportsShouldProcess=$True)]
+    param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Path,
+        [Parameter(Mandatory=$true,Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [Int]
+        $InitialSize,
+        [Parameter(Mandatory=$true,Position=2)]
+        [ValidateNotNullOrEmpty()]
+        [Int]
+        $MaximumSize
+    )
+    Set-PSDebug -Strict
+ 
+    $ComputerSystem = $null
+    $CurrentPageFile = $null
+    $Modified = $false
+ 
+    # Disables automatically managed page file setting first
+    $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem -EnableAllPrivileges
+    if ($ComputerSystem.AutomaticManagedPagefile)
+    {
+        $ComputerSystem.AutomaticManagedPagefile = $false
+        if ($PSCmdlet.ShouldProcess("$($ComputerSystem.Path.Server)", 'Disable automatic managed page file'))
+        {
+            $ComputerSystem.Put()
+        }
+    }
+ 
+    $CurrentPageFile = Get-WmiObject -Class Win32_PageFileSetting
+    if ($CurrentPageFile.Name -eq $Path)
+    {
+        # Keeps the existing page file
+        if ($CurrentPageFile.InitialSize -ne $InitialSize)
+        {
+            $CurrentPageFile.InitialSize = $InitialSize
+            $Modified = $true
+        }
+        if ($CurrentPageFile.MaximumSize -ne $MaximumSize)
+        {
+            $CurrentPageFile.MaximumSize = $MaximumSize
+            $Modified = $true
+        }
+        if ($Modified)
+        {
+            if ($PSCmdlet.ShouldProcess("Page file $Path", "Set initial size to $InitialSize and maximum size to $MaximumSize"))
+            {
+                $CurrentPageFile.Put()
+            }
+        }
+    }
+    else
+    {
+        # Creates a new page file
+        if ($PSCmdlet.ShouldProcess("Page file $($CurrentPageFile.Name)", 'Delete old page file'))
+        {
+            $CurrentPageFile.Delete()
+        }
+        if ($PSCmdlet.ShouldProcess("Page file $Path", "Set initial size to $InitialSize and maximum size to $MaximumSize"))
+        {
+            Set-WmiInstance -Class Win32_PageFileSetting -Arguments @{Name=$Path; InitialSize = $InitialSize; MaximumSize = $MaximumSize}
+        }
+    }
+}
+
+Set-PageFile -path C:\pagefile.sys -InitialSize 1000 -MaximumSize 2000
+
+Remove-Partition -DriveLetter D -Confirm:$false
+get-disk -Number 1 | Set-Disk –IsOffline $True
+
+restart-computer -force
+
+
+
